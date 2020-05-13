4f9932fbf0b5cded76e71ab3d3a2d2203422e532
user <user@gmail.com>
2020-05-13 12:28:35 +0530

    ready for production

--- server.js
+++ server.js
@@ -4,6 +4,7 @@ const bodyParser = require("body-parser");
 const mongoose = require("mongoose");
 const adminRoutes = require("./routes/admin");
 const jobRoutes = require("./routes/job");
+const path = require("path");
 
 const app = express();
 
@@ -26,6 +27,14 @@ mongoose
 
 app.use(adminRoutes);
 app.use(jobRoutes);
+
+if (process.env.NODE_ENV === "production") {
+  app.use(express.static("client/build"));
+
+  app.get("*", (req, res) => {
+    res.sendFile(path.resolve(__dirname, "client", "build", "index.html"));
+  });
+}
 
 app.listen(port, () => {
   console.log(`Server is up and running at ${port}`);
